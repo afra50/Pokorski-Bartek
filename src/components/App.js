37a2f47c460e137204f985ef332d937ef0ccd0cf
Emilia <afra57@onet.pl>
2025-01-24 01:39:45 +0100

    szkic

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../styles/App.scss";
+
+function App() {
+  return <div>Hello!</div>;
+}
+
+export default App;
